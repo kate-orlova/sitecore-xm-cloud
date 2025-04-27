cab601c9d7ee52a4fabb7d7b8024a50abb24b789
Kate Orlova <kate.s.orlova@gmail.com>
2025-04-27 13:54:00 +0100

    GatedContent component example

---
+++ src/nextjs/src/components/GatedContent.tsx
@@ -0,0 +1,12 @@
+import GatedContentWrapper from "./GatedContentWrapper";
+
+export default function GatedContent() {
+  return (
+    <GatedContentWrapper>
+      <div className="component-content">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur nec velit ut tellus mollis temspor ac nec tellus. Praesent mollis finibus urna. Integer tempus ac metus congue blandit. Mauris quam libero, porttitor quis velit a, lobortis cursus libero. Morbi sed nunc sed lectus mollis pretium. Nulla id dignissim arcu. Nam nec est ultricies, fringilla elit sit amet, pretium nunc. Mauris interdum mauris eu ligula semper, sit amet pulvinar felis facilisis. Pellentesque hendrerit dui et mauris fringilla, eget porta augue convallis. Fusce consequat eu eros et sagittis. Phasellus sit amet odio vel libero condimentum cursus.</p>
+        <p>Pellentesque malesuada, quam varius scelerisque varius, sem arcu ultricies neque, in aliquam nulla sem vel arcu. Duis blandit, erat sed fringilla semper, arcu lectus interdum enim, et rutrum magna diam id nunc. Nunc ac orci at diam gravida mollis sed ac leo. Aliquam erat volutpat. Aliquam massa ante, posuere a nisl ut, consectetur malesuada felis. Vivamus congue sodales tortor. Sed ut lacus sit amet eros consectetur laoreet rutrum eu arcu. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc sed gravida enim. Etiam at massa sollicitudin, vulputate nisi quis, porttitor magna. Cras nec commodo tortor. Sed id orci erat.</p>
+      </div>
+    </GatedContentWrapper>
+  )
+}
